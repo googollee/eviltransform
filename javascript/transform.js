cd2b08e62726dbfded58433dfa159156ac34e6b5
Mingye Wang <arthur200126@gmail.com>
2016-01-31 06:36:50 -0500

    js: make gcj stuffs sane
    Just some renaming for internal funcs.

--- javascript/transform.js
+++ javascript/transform.js
@@ -15,27 +15,30 @@ function outOfChina(lat, lng) {
 	return false;
 }
 
-function transformLat(x, y) {
-	var ret = -100.0 + 2.0*x + 3.0*y + 0.2*y*y + 0.1*x*y + 0.2*Math.sqrt(Math.abs(x));
-	ret += (20.0*Math.sin(6.0*x*Math.PI) + 20.0*Math.sin(2.0*x*Math.PI)) * 2.0 / 3.0;
-	ret += (20.0*Math.sin(y*Math.PI) + 40.0*Math.sin(y/3.0*Math.PI)) * 2.0 / 3.0;
-	ret += (160.0*Math.sin(y/12.0*Math.PI) + 320*Math.sin(y*Math.PI/30.0)) * 2.0 / 3.0;
+function gcjTrigLat(x, y) {
+	var ret = 20.0*Math.sin(6.0*x*Math.PI) + 20.0*Math.sin(2.0*x*Math.PI);
+	ret += 20.0*Math.sin(y*Math.PI) + 40.0*Math.sin(y/3.0*Math.PI);
+	ret += 160.0*Math.sin(y/12.0*Math.PI) + 320*Math.sin(y*Math.PI/30.0);
+	ret *= 2.0 / 3.0;
+	ret += -100.0 + 2.0*x + 3.0*y + 0.2*y*y + 0.1*x*y + 0.2*Math.sqrt(Math.abs(x));
 	return ret;
 }
 
-function transformLon(x, y) {
-	var ret = 300.0 + x + 2.0*y + 0.1*x*x + 0.1*x*y + 0.1*Math.sqrt(Math.abs(x));
-	ret += (20.0*Math.sin(6.0*x*Math.PI) + 20.0*Math.sin(2.0*x*Math.PI)) * 2.0 / 3.0;
-	ret += (20.0*Math.sin(x*Math.PI) + 40.0*Math.sin(x/3.0*Math.PI)) * 2.0 / 3.0;
-	ret += (150.0*Math.sin(x/12.0*Math.PI) + 300.0*Math.sin(x/30.0*Math.PI)) * 2.0 / 3.0;
+function gcjTrigLon(x, y) {
+	var ret = 20.0*Math.sin(6.0*x*Math.PI) + 20.0*Math.sin(2.0*x*Math.PI);
+	ret += 20.0*Math.sin(x*Math.PI) + 40.0*Math.sin(x/3.0*Math.PI);
+	ret += 150.0*Math.sin(x/12.0*Math.PI) + 300.0*Math.sin(x/30.0*Math.PI);
+	ret *= 2.0 / 3.0;
+	ret += 300.0 + x + 2.0*y + 0.1*x*x + 0.1*x*y + 0.1*Math.sqrt(Math.abs(x));
 	return ret;
 }
 
-function delta(lat, lng) {
+// wgs -> (gcj - wgs), exact
+function gcjDelta(lat, lng) {
 	var a = 6378245.0;
 	var ee = 0.00669342162296594323;
-	var dLat = transformLat(lng-105.0, lat-35.0);
-	var dLng = transformLon(lng-105.0, lat-35.0);
+	var dLat = gcjTrigLat(lng-105.0, lat-35.0);
+	var dLng = gcjTrigLon(lng-105.0, lat-35.0);
 	var radLat = lat / 180.0 * Math.PI;
 	var magic = Math.sin(radLat);
 	magic = 1 - ee*magic*magic;
@@ -49,7 +52,7 @@ function wgs2gcj(wgsLat, wgsLng) {
 	if (outOfChina(wgsLat, wgsLng)) {
 		return {"lat": wgsLat, "lng": wgsLng};
 	}
-	var d = delta(wgsLat, wgsLng);
+	var d = gcjDelta(wgsLat, wgsLng);
 	return {"lat": wgsLat + d.lat, "lng": wgsLng + d.lng};
 }
 exports.wgs2gcj = wgs2gcj;
@@ -58,7 +61,8 @@ function gcj2wgs(gcjLat, gcjLng) {
 	if (outOfChina(gcjLat, gcjLng)) {
 		return {"lat": gcjLat, "lng": gcjLng};
 	}
-	var d = delta(gcjLat, gcjLng);
+	// since gcj \approx wgs, let's just use this one..
+	var d = gcjDelta(gcjLat, gcjLng);
 	return {"lat": gcjLat - d.lat, "lng": gcjLng - d.lng};
 }
 exports.gcj2wgs = gcj2wgs;
@@ -114,6 +118,7 @@ function gcj2bd(gcjLat, gcjLng) {
 }
 exports.gcj2bd = gcj2bd;
 
+// TODO: error approx?
 function bd2gcj(bdLat, bdLng) {
 	if (outOfChina(bdLat, bdLng)) {
 		return {"lat": bdLat, "lng": bdLng};
