cd7a6a43095c5fb54dc872f0cf6bd189519900f2
Googol Lee <i@googol.im>
2016-06-01 00:24:11 +0800

    optimize code

--- c/transform.c
+++ c/transform.c
@@ -12,7 +12,7 @@
 
 #define fabs(x) __ev_fabs(x)
 /* do not use >=, or compiler may not know to optimize this with a simple (x & 0x80...0). */
-INLINE static double __ev_fabs(x){ return x > 0.0 ? x : -x; }
+INLINE static double __ev_fabs(double x){ return x > 0.0 ? x : -x; }
 
 INLINE static int outOfChina(double lat, double lng) {
 	if (lng < 72.004 || lng > 137.8347) {
@@ -27,19 +27,24 @@ INLINE static int outOfChina(double lat, double lng) {
 void transform(double x, double y, double *lat, double *lng) {
 	double xy = x * y;
 	double absX = sqrt(fabs(x));
-	double d = (20.0*sin(6.0*x*M_PI) + 20.0*sin(2.0*x*M_PI)) * 2.0 / 3.0;
+	double xPi = x * M_PI;
+	double yPi = y * M_PI;
+	double d = 20.0*sin(6.0*xPi) + 20.0*sin(2.0*xPi);
 
-	*lat = -100.0 + 2.0*x + 3.0*y + 0.2*y*y + 0.1*xy + 0.2*absX;
-	*lng = 300.0 + x + 2.0*y + 0.1*x*x + 0.1*xy + 0.1*absX;
+	*lat = d;
+	*lng = d;
 
-	*lat += d;
-	*lng += d;
+	*lat += 20.0*sin(yPi) + 40.0*sin(yPi/3.0);
+	*lng += 20.0*sin(xPi) + 40.0*sin(xPi/3.0);
 
-	*lat += (20.0*sin(y*M_PI) + 40.0*sin(y/3.0*M_PI)) * 2.0 / 3.0;
-	*lng += (20.0*sin(x*M_PI) + 40.0*sin(x/3.0*M_PI)) * 2.0 / 3.0;
+	*lat += 160.0*sin(yPi/12.0) + 320*sin(yPi/30.0);
+	*lng += 150.0*sin(xPi/12.0) + 300.0*sin(xPi/30.0);
 
-	*lat += (160.0*sin(y/12.0*M_PI) + 320*sin(y/30.0*M_PI)) * 2.0 / 3.0;
-	*lng += (150.0*sin(x/12.0*M_PI) + 300.0*sin(x/30.0*M_PI)) * 2.0 / 3.0;
+	*lat *= 2.0 / 3.0;
+	*lng *= 2.0 / 3.0;
+
+	*lat += -100.0 + 2.0*x + 3.0*y + 0.2*y*y + 0.1*xy + 0.2*absX;
+	*lng += 300.0 + x + 2.0*y + 0.1*x*x + 0.1*xy + 0.1*absX;
 }
 
 static void delta(double lat, double lng, double *dLat, double *dLng) {
@@ -88,37 +93,51 @@ void gcj2wgs(double gcjLat, double gcjLng, double *wgsLat, double *wgsLng) {
 }
 
 void gcj2wgs_exact(double gcjLat, double gcjLng, double *wgsLat, double *wgsLng) {
-	double dLat, dLng;
-	// n_iter=2: centimeter precision, n_iter=5: double precision
-	const int n_iter = 2;
+	double initDelta = 0.01;
+	double threshold = 0.000001;
+	double dLat, dLng, mLat, mLng, pLat, pLng;
+	dLat = dLng = initDelta;
+	mLat = gcjLat - dLat;
+	mLng = gcjLng - dLng;
+	pLat = gcjLat + dLat;
+	pLng = gcjLng + dLng;
 	int i;
-	if ((wgsLat == NULL) || (wgsLng == NULL)) {
-		return;
+	for (i=0; i<30; i++) {
+		*wgsLat = (mLat+pLat) / 2;
+		*wgsLng = (mLng+pLng) / 2;
+		double tmpLat, tmpLng;
+		wgs2gcj(*wgsLat, *wgsLng, &tmpLat, &tmpLng);
+		dLat = tmpLat - gcjLat;
+		dLng = tmpLng - gcjLng;
+		if ( (fabs(dLat)<threshold) && (fabs(dLng)<threshold) ) {
+			return;
+		}
+		if (dLat > 0) {
+			pLat = *wgsLat;
+		} else {
+			mLat = *wgsLat;
+		}
+		if (dLng > 0) {
+			pLng = *wgsLng;
+		} else {
+			mLng = *wgsLng;
+		}
 	}
-	*wgsLat = gcjLat;
-	*wgsLng = gcjLng;
-	if (outOfChina(gcjLat, gcjLng)) {
-		return;
-	}
-	for (i = 0; i < n_iter; i++) {
-		delta(*wgsLat, *wgsLng, &dLat, &dLng);
-		*wgsLat = gcjLat - dLat;
-		*wgsLng = gcjLng - dLng;
-	}
-}
-
-// 1 - cos(x) == 2 sin^2(x/2)
-INLINE static double oneMinusCos(double x)
-{
-	double s = sin(x/2);
-	return s*s*2;
 }
 
 double distance(double latA, double lngA, double latB, double lngB) {
 	const double earthR = 6371000;
-	latA *= M_PI/180;
-	latB *= M_PI/180;
-	lngA *= M_PI/180;
-	lngB *= M_PI/180;
-	return 2*earthR*asin(sqrt(oneMinusCos(latA-latB) + cos(latA)*cos(latB)*(oneMinusCos(lngA - lngB)))/M_SQRT2);
+	double arcLatA = latA * M_PI/180;
+	double arcLatB = latB * M_PI/180;
+	double x = cos(arcLatA) * cos(arcLatB) * cos((lngA-lngB)*M_PI/180);
+	double y = sin(arcLatA) * sin(arcLatB);
+	double s = x + y;
+	if (s > 1) {
+		s = 1;
+	}
+	if (s < -1) {
+		s = -1;
+	}
+	double alpha = acos(s);
+	return alpha * earthR;
 }
