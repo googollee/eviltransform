6091f17009a2abb19ab959bafaac71e09763752d
Googol Lee <i@googol.im>
2016-06-10 15:14:26 +0800

    update javascript

--- javascript/test.js
+++ javascript/test.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var transform = require('./transform.js');
 
 var tests = [
--- javascript/transform.js
+++ javascript/transform.js
@@ -1,4 +1,5 @@
 "use strict";
+
 var exports;
 if (typeof module === "object" && exports) {
 	exports = module.exports;
@@ -16,86 +17,107 @@ function outOfChina(lat, lng) {
 	return false;
 }
 
-function gcjTrigLat(x, y) {
-	var ret = 20.0*Math.sin(6.0*x*Math.PI) + 20.0*Math.sin(2.0*x*Math.PI);
-	ret += 20.0*Math.sin(y*Math.PI) + 40.0*Math.sin(y/3.0*Math.PI);
-	ret += 160.0*Math.sin(y/12.0*Math.PI) + 320*Math.sin(y*Math.PI/30.0);
-	ret *= 2.0 / 3.0;
-	ret += -100.0 + 2.0*x + 3.0*y + 0.2*y*y + 0.1*x*y + 0.2*Math.sqrt(Math.abs(x));
-	return ret;
+function transform(x, y) {
+	var xy = x * y;
+	var absX = Math.sqrt(Math.abs(x));
+	var xPi = x * Math.PI;
+	var yPi = y * Math.PI;
+	var d = 20.0*Math.sin(6.0*xPi) + 20.0*Math.sin(2.0*xPi);
+
+	var lat = d;
+	var lng = d;
+
+	lat += 20.0*Math.sin(yPi) + 40.0*Math.sin(yPi/3.0);
+	lng += 20.0*Math.sin(xPi) + 40.0*Math.sin(xPi/3.0);
+
+	lat += 160.0*Math.sin(yPi/12.0) + 320*Math.sin(yPi/30.0);
+	lng += 150.0*Math.sin(xPi/12.0) + 300.0*Math.sin(xPi/30.0);
+
+	lat *= 2.0 / 3.0;
+	lng *= 2.0 / 3.0;
+
+	lat += -100.0 + 2.0*x + 3.0*y + 0.2*y*y + 0.1*xy + 0.2*absX;
+	lng += 300.0 + x + 2.0*y + 0.1*x*x + 0.1*xy + 0.1*absX;
+
+	return {lat: lat, lng: lng}
 }
 
-function gcjTrigLon(x, y) {
-	var ret = 20.0*Math.sin(6.0*x*Math.PI) + 20.0*Math.sin(2.0*x*Math.PI);
-	ret += 20.0*Math.sin(x*Math.PI) + 40.0*Math.sin(x/3.0*Math.PI);
-	ret += 150.0*Math.sin(x/12.0*Math.PI) + 300.0*Math.sin(x/30.0*Math.PI);
-	ret *= 2.0 / 3.0;
-	ret += 300.0 + x + 2.0*y + 0.1*x*x + 0.1*x*y + 0.1*Math.sqrt(Math.abs(x));
-	return ret;
-}
-
-// wgs -> (gcj - wgs), exact
-function gcjDelta(lat, lng) {
+function delta(lat, lng) {
 	var a = 6378245.0;
 	var ee = 0.00669342162296594323;
-	var dLat = gcjTrigLat(lng-105.0, lat-35.0);
-	var dLng = gcjTrigLon(lng-105.0, lat-35.0);
+	var d = transform(lng-105.0, lat-35.0);
 	var radLat = lat / 180.0 * Math.PI;
 	var magic = Math.sin(radLat);
 	magic = 1 - ee*magic*magic;
 	var sqrtMagic = Math.sqrt(magic);
-	dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * Math.PI);
-	dLng = (dLng * 180.0) / (a / sqrtMagic * Math.cos(radLat) * Math.PI);
-	return {"lat": dLat, "lng": dLng};
+	d.lat = (d.lat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * Math.PI);
+	d.lng = (d.lng * 180.0) / (a / sqrtMagic * Math.cos(radLat) * Math.PI);
+	return d;
 }
 
 function wgs2gcj(wgsLat, wgsLng) {
 	if (outOfChina(wgsLat, wgsLng)) {
-		return {"lat": wgsLat, "lng": wgsLng};
+		return {lat: wgsLat, lng: wgsLng};
 	}
-	var d = gcjDelta(wgsLat, wgsLng);
-	return {"lat": wgsLat + d.lat, "lng": wgsLng + d.lng};
+	var d = delta(wgsLat, wgsLng);
+	return {lat: wgsLat + d.lat, lng: wgsLng + d.lng};
 }
 exports.wgs2gcj = wgs2gcj;
 
 function gcj2wgs(gcjLat, gcjLng) {
 	if (outOfChina(gcjLat, gcjLng)) {
-		return {"lat": gcjLat, "lng": gcjLng};
+		return {lat: gcjLat, lng: gcjLng};
 	}
-	// since gcj \approx wgs, let's just use this one..
-	var d = gcjDelta(gcjLat, gcjLng);
-	return {"lat": gcjLat - d.lat, "lng": gcjLng - d.lng};
+	var d = delta(gcjLat, gcjLng);
+	return {lat: gcjLat - d.lat, lng: gcjLng - d.lng};
 }
 exports.gcj2wgs = gcj2wgs;
 
 function gcj2wgs_exact(gcjLat, gcjLng) {
-	// newCoord = oldCoord = gcjCoord
-	var newLat = gcjLat, newLng = gcjLng;
-	var oldLat = newLat, oldLng = newLng;
-	var threshold = 1e-6; // ~0.55 m equator & latitude
-	
-	for (var i = 0; i < 30 && ; i++) {
-		// oldCoord = newCoord
-		oldLat = newLat;
-		oldLng = newLng;
-		// newCoord = gcjCoord - wgs_to_gcj_delta(newCoord)
-		var gcjDiff = gcjDelta(newLat,newLng);
-		newLat = gcjLat - gcjDiff.lat;
-		newLng = gcjLng - gcjDiff.lng;
-		// diffchk
-		if (Math.max(Math.abs(oldLat - newLat), Math.abs(oldLng - newLng)) < threshold) {
-			break;
+	var initDelta = 0.01;
+	var threshold = 0.000001;
+	var dLat = initDelta, dLng = initDelta;
+	var mLat = gcjLat-dLat, mLng = gcjLng-dLng;
+	var pLat = gcjLat+dLat, pLng = gcjLng+dLng;
+	var wgsLat, wgsLng;
+	for (var i = 0; i < 30; i++) {
+		wgsLat = (mLat+pLat)/2;
+		wgsLng = (mLng+pLng)/2;
+		var tmp = wgs2gcj(wgsLat, wgsLng)
+		dLat = tmp.lat-gcjLat;
+		dLng = tmp.lng-gcjLng;
+		if ((Math.abs(dLat) < threshold) && (Math.abs(dLng) < threshold)) {
+			return {lat: wgsLat, lng: wgsLng};
+		}
+		if (dLat > 0) {
+			pLat = wgsLat;
+		} else {
+			mLat = wgsLat;
+		}
+		if (dLng > 0) {
+			pLng = wgsLng;
+		} else {
+			mLng = wgsLng;
 		}
 	}
-	return {"lat": wgsLat, "lng": wgsLng};
+	return {lat: wgsLat, lng: wgsLng};
 }
 exports.gcj2wgs_exact = gcj2wgs_exact;
 
 function distance(latA, lngA, latB, lngB) {
 	var earthR = 6371000;
-	var x = Math.cos(latA*Math.PI/180) * Math.cos(latB*Math.PI/180) * Math.cos((lngA-lngB)*Math.PI/180);
-	var y = Math.sin(latA*Math.PI/180) * Math.sin(latB*Math.PI/180);
-	var s = Math.max(Math.min(x + y, 1), -1);
+	var pi180 = Math.PI / 180;
+	var arcLatA = latA * pi180;
+ 	var arcLatB = latB * pi180;
+	var x = Math.cos(arcLatA) * Math.cos(arcLatB) * Math.cos((lngA-lngB)*pi180);
+	var y = Math.sin(arcLatA) * Math.sin(arcLatB);
+	var s = x + y;
+	if (s > 1) {
+		s = 1;
+	}
+	if (s < -1) {
+		s = -1;
+	}
 	var alpha = Math.acos(s);
 	var distance = alpha * earthR;
 	return distance;
@@ -104,7 +126,7 @@ exports.distance = distance;
 
 function gcj2bd(gcjLat, gcjLng) {
 	if (outOfChina(gcjLat, gcjLng)) {
-		return {"lat": gcjLat, "lng": gcjLng};
+		return {lat: gcjLat, lng: gcjLng};
 	}
 
 	var x = gcjLng, y = gcjLat;
@@ -112,14 +134,13 @@ function gcj2bd(gcjLat, gcjLng) {
 	var theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * Math.PI);
 	var bdLng = z * Math.cos(theta) + 0.0065;
 	var bdLat = z * Math.sin(theta) + 0.006;
-	return {"lat": bdLat, "lng": bdLng};
+	return {lat: bdLat, lng: bdLng};
 }
 exports.gcj2bd = gcj2bd;
 
-// TODO: error approx?
 function bd2gcj(bdLat, bdLng) {
 	if (outOfChina(bdLat, bdLng)) {
-		return {"lat": bdLat, "lng": bdLng};
+		return {lat: bdLat, lng: bdLng};
 	}
 
 	var x = bdLng - 0.0065, y = bdLat - 0.006;
@@ -127,7 +148,7 @@ function bd2gcj(bdLat, bdLng) {
 	var theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * Math.PI);
 	var gcjLng = z * Math.cos(theta);
 	var gcjLat = z * Math.sin(theta);
-	return {"lat": gcjLat, "lng": gcjLng};
+	return {lat: gcjLat, lng: gcjLng};
 }
 exports.bd2gcj = bd2gcj;
 
