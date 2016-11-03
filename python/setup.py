76106d02a61e5d432e56e5d5dbd6585113d87996
Di Weng <mystery.wd@gmail.com>
2016-11-03 16:31:36 +0800

    publish python package to PyPI

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'eviltransform',
+  packages = [ 'eviltransform' ],
+  version = '0.1.0',
+  description = 'Transform coordinates between earth(WGS-84) and mars in china(GCJ-02).',
+  author = 'googollee, et. al',
+  url = 'https://github.com/googollee/eviltransform',
+  keywords = [ 'gis' ]
+)
+
